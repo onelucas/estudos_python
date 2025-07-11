3d016e0a99464517e78d1c0e98587903f670df74
onelucas <lucasviniciusmartins2004@gmail.com>
2025-07-11 17:14:49 -0300

    Adição de cad_user e Login

--- TEP-main/semana9/core/urls.py
+++ TEP-main/semana9/core/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('del_livro/<int:pk>', views.del_livro, name='deletar_livro_url'),
     path('upg_livro/<int:pk>', views.upg_livro, name='atualizar_livro_url'),
     path('cad_livro', views.cadastrar_livro, name='cadastrar_livro_url'),
-    path('avaliar_textos', views.avaliar_textos, name='avaliar_textos_url'),
+    path('cad_user', views.cad_user, name='cad_user_url'),
+    path('entrar', views.entrar, name='entrar_url'),
+    path('sair', views.sair, name='sair_url'),
+    path('graf', views.graf, name='graf_url'),
+    # path('avaliar_textos', views.avaliar_textos, name='avaliar_textos_url'),
 ]
--- TEP-main/semana9/core/views.py
+++ TEP-main/semana9/core/views.py
@@ -1,6 +1,43 @@
 from django.shortcuts import render, redirect
 from .models import Livro
+from django.contrib.auth.models import User
+from django.http.response import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+import matplotlib.pyplot as plt
+import io
+import base64
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+def graf(request):
+    livros = Livro.objects.all()
+    df = pd.DataFrame.from_records(livros.values())
+
+    df['price'] = pd.to_numeric(df['price'], errors='coerce')
+    df['review_score'] = pd.to_numeric(df['review_score'], errors='coerce')
+
+    df_grouped = df.groupby('review_score') ['price'].mean().reset_index()
+
+    fig = px.bar(
+        df_grouped,
+        x = 'review_score',
+        y = 'price',
+        title = 'Preço Médio por Avaliação',
+        labels = {'review_score': 'Avaliação', 'price': 'Preço Médio (R$)'},
+        template = 'plotly_white'
+    )
+
+    plot_div = fig.to_html(full_html=False)
+    context = {
+        'plot_div': plot_div
+    }
+    return render(request, 'graf.html', context)
+
+
+@login_required(login_url='entrar_url')
 def index(request):
     livros = Livro.objects.all()
     context = {
@@ -40,8 +77,9 @@ def upg_livro(request, pk):
         return redirect('index_url')
 
 def cadastrar_livro(request):
-    if request.method == 'GET':
-        return render(request, 'cad_livro.html')
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            return render(request, 'cad_livro.html')
     elif request.method == 'POST':
         titulo = request.POST.get('titulo')
         preco = request.POST.get('preco')
@@ -54,3 +92,36 @@ def cadastrar_livro(request):
         livro.save()
         return redirect('index_url')
     
+    else:
+        return redirect('entrar_url')
+    
+def cad_user(request):
+    if request.method == 'GET':
+        return render(request, 'cad_user.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = User.objects.create_user(username=username, password=password, email=email)  
+        user.save()
+        #return HttpResponse('Sucesso ao Cadastrar')
+        return redirect ('entrar_url')
+
+def entrar(request):
+    if request.method == 'GET':
+        return render(request, 'entrar.html')
+    else:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('index_url')
+        else:
+            return HttpResponse('Falha ao tentar autenticar')
+    
+def sair(request):
+    logout(request)
+    return redirect('entrar_url')
